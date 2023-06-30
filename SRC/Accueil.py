3d27e520fe0a0d1d2163bcc83f90a3b41e77d94c
Alexis M <datalexis0@gmail.com>
2023-06-30 09:53:57 +0200

    Add SRC + CSVs + PYs + Logos + Présentation CANVA

---
+++ SRC/Accueil.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import pandas as pd
+from PIL import Image
+
+#Configuration des dimensions & affichage de la page
+st.set_page_config(page_title="Emmaüs Connect Project",
+                   page_icon=":computer:",
+                   layout='wide')
+
+#Importation image accueil
+image = Image.open('C:/Users/murai/OneDrive/Bureau/DATA/Datathon2/Logo_Emmaüs_Connect.png')
+
+# Initialisation du fond d'écran
+import base64
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+add_bg_from_local('C:/Users/murai/OneDrive/Bureau/DATA/Datathon2/Logo_Emmaüs_Connect.png')
