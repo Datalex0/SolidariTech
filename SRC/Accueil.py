06594437656bfc246f0209536e68978e1b84e2d1
Alexis <126944670+Datalex0@users.noreply.github.com>
2023-07-03 10:17:36 +0200

    Update Accueil.py

--- SRC/Accueil.py
+++ SRC/Accueil.py
@@ -28,4 +28,20 @@ def add_bg_from_local(image_file):
     )
 add_bg_from_local('SRC/Logo_Emmaüs_Connect.png')
 
-st.sidebar.write("")
+
+# TITRE DEFILANT
+# Définition du texte à afficher
+texte = "SolidariTech"
+# Configuration de la vitesse de défilement (en pixels par seconde)
+vitesse = 20
+# Génération du code HTML pour le texte défilant
+html_code = f"""<marquee scrollamount="{vitesse}"> {texte} </marquee>"""
+# Mise en forme taille = 150 pixels
+st.markdown("""<style>
+        .big-font {
+            font-size:150px
+        }
+        </style>""", unsafe_allow_html=True)
+# Affichage du texte défilant
+st.markdown(f'<p class="big-font"><font face="Brush Script MT">{html_code}</font></p>', unsafe_allow_html=True)
+
