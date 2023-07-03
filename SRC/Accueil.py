0e71f8d2a0afb8b58a2afcaddec4685a74a9ef7c
Alexis <126944670+Datalex0@users.noreply.github.com>
2023-07-03 10:12:43 +0200

    Update Accueil.py

--- SRC/Accueil.py
+++ SRC/Accueil.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 from PIL import Image
+import streamlit.components.v1 as components
 
 #Configuration des dimensions & affichage de la page
 st.set_page_config(page_title="Emmaüs Connect Project",
